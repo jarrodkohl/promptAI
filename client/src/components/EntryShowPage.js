f1e2fa3e407da5f0a55033746a65b0078da211ef
Jarrod Kohl <jarrod.kohl@gmail.com>
2023-02-25 01:20:35 -0500

    Added edit to prompts and icons

--- client/src/components/EntryShowPage.js
+++ client/src/components/EntryShowPage.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { FaTrash } from 'react-icons/fa'
 
 const EntryShowPage = ({ match }) => {
   const [entry, setEntry] = useState({
@@ -6,6 +8,9 @@ const EntryShowPage = ({ match }) => {
     entryContent: "",
     label: ""
   });
+
+  const [isDeleted, setIsDeleted] = useState(false)
+
 
   const promptId = match.params.promptId
   const entryId = match.params.entryId
@@ -29,13 +34,37 @@ const EntryShowPage = ({ match }) => {
     getEntry()
   }, [])
 
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`/api/v1/prompts/${promptId}/entries/${entryId}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw error;
+      }
+      setIsDeleted(true);
+    } catch (error) {
+      console.error(`Error in fetch: ${error.message}`)
+    }
+  }
+
+  if (isDeleted) {
+    return <Redirect to={`/prompts/${promptId}`} />
+  }
+
   return (
     <div className="callout">
       <h2 className="show-title">{entry.title}</h2>
       <p className="show-label">{entry.label}</p>
       <p className="show-entry-content">{entry.entryContent}</p>
+      <button className="delete-btn" onClick={handleDelete}>
+      <FaTrash size={16} color="black" />
+      </button>
+
     </div>
-  );
-};
+  )
+}
 
 export default EntryShowPage;
