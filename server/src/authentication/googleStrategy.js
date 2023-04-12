fce5c6faea29fe7bda5d321e8ef40058d7735950
Jarrod Kohl <jarrod.kohl@gmail.com>
2023-04-12 11:46:13 -0400

    Auth_Config Bug Fix

--- server/src/authentication/googleStrategy.js
+++ server/src/authentication/googleStrategy.js
@@ -23,7 +23,7 @@ const googleAuthHandler = (accessToken, refreshToken, profile, done) =>{
 const googleStrategy = new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: process.env.CALLBACK_URL,
   },
   googleAuthHandler
   )
